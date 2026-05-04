7181b5aca6662bc4a6a166a5469d6f3e70c0a6ff
ikboljonabdumalikov <happinessjonabdumalikov@gmail.com>
2026-05-05 01:58:17 +0900

    add switch condition logic to the project

--- Program.cs
+++ Program.cs
@@ -15,3 +15,33 @@ double firstNumber = Convert.ToDouble(Console.ReadLine());
 
 Console.WriteLine("Enter the second number: ");
 double secondNumber = Convert.ToDouble(Console.ReadLine());
+
+switch (arithmeticOption)
+{
+    case "1":
+
+        Console.WriteLine($"{firstNumber + secondNumber}");
+        break;
+
+    case "2":
+
+        Console.WriteLine($"{firstNumber - secondNumber}");
+        break;
+
+    case "3":
+
+        Console.WriteLine($"{firstNumber * secondNumber}");
+        break;
+
+    case "4":
+
+        if(secondNumber == 0) {
+            Console.WriteLine("It's not possible to divide 0");
+        } else {
+            Console.WriteLine($"{firstNumber / secondNumber}");
+        }
+        break;
+    default:
+    Console.WriteLine("Something went wrong. Please try again.");
+    break;
+}
